fb7daedd52c83b7acddfcad933927076e5d1b2a5
LuisF14 <luisff98.al@gmail.com>
2022-06-18 01:02:31 -0500

    NEW

--- Auditor/COBIT.php
+++ Auditor/COBIT.php
@@ -20,19 +20,28 @@
 					<ul id="encabezado_cobit" class="encabezado">
 						<li><a href="#dominio">Dominio</a></li>
 						<li><a href="#procesos">Procesos</a></li>
-                        <li><a href="#controles">Controles</a></li>
+                        <li><a href="#controles">Prácticas</a></li>
 					</ul>
 					<div class="contenido" id="contenido_cobit">
 						<!-- MENU DOMINIO -->
 						<div id="dominio">
-							<div class="item">
+						    <div class="item">
+							    <div class="col izq">
+								    <h3>Entrega, Servicio y Soporte</h3>
+							    </div>
+						    </div>	
+						    <div class="item">
 								<div class="col izq">
-									<h3>Lorem ipsum dolor.</h3>
-									<p>Lorem ipsum dolor, sit amet consectetur, adipisicing elit.</p>
+									<!-- <h3>Entrega, Servicio y Soporte</h3> -->
+									<p>Este dominio pretende lograr que los servicios de TI 
+										se entreguen de acuerdo con las prioridades del negocio 
+										a fin de asegurar que la fuerza de trabajo utilice los sistemas 
+										de información de modo productivo y seguro. Además, permite  
+										implantar de forma correcta la confidencialidad, integridad y disponibilidad.</p>
 								</div>	
 							</div>
 
-							<div class="item">
+							<!-- <div class="item">
 								<div class="col izq">
 									<h3>Donec placerat dignissin.</h3>
 									<p>Macenas iaculis aliquam lectus, coutywe.</p>
@@ -51,71 +60,379 @@
 									<h3>Sed semper.</h3>
 									<p> Voluptatibus magni amet laborum tenetur, eaque unde.</p>
 								</div>	
-							</div>
+							</div> -->
 						</div>
 					<!-- FIN DOMINIO -->
 
 					<!-- PROCESOS -->
-
 					<div id="procesos">
 						<div class="item">
 							<div class="col izq">
-								<h3>Cafe ipsum dolor</h3>
-								<p>Ut sunt veniam praesentium illum possimus autem eaque est quod labore error.Totam dolorum veritatis dignissimos placeat voluptates.</p>
+								<h3>Procesos</h3>
 							</div>
 						</div>
 
 						<div class="item">
 							<div class="col izq">
-								<h3>Late dignissim</h3>
-								<p>Aliquid beatae explicabo at officiis corrupti libero nemo alias molestiae.Ducimus similique dolor, perferendis pariatur omnis. </p>
+								<h3>DSS01. Gestionar operaciones</h3>
+								<p>Coordinar y ejecutar las actividades y los procedimientos 
+									operativos requeridos para entregar servicios de TI.</p>
 							</div>
 						</div>
 
 						<div class="item">
 							<div class="col izq">
-								<h3>Capuccino vestibulum</h3>
-								<p>Quos necessitatibus deserunt laudantium, tempora quibusdam? Maxime maiores consectetur aliquam Aliquid beatae explicabo at officiis corrupti libero nemo alias molestiae perferendis.</p>
+								<h3>DSS02. Gestionar peticiones e incidentes de servicio</h3>
+								<p>Proveer una respuesta oportuna y efectiva a las peticiones 
+									de usuario y la resolución de todo tipo de incidentes como 
+									registrar, investigar, diagnosticar, escalar y resolver incidentes.</p>
 							</div>
 						</div>
 
 						<div class="item">
 							<div class="col izq">
-								<h3>Cafe semper</h3>
-								<p>Pariatur omnis, quos necessitatibus deserunt laudantium, tempora quibu.explicabo at officiis corrupti libero nemo alias molestiae perferendis.</p>
+								<h3>DSS03. Gestionar problemas</h3>
+								<p>Identificar y clasificar problemas y proporcionar resolución 
+									en tiempo para prevenir incidentes recurrentes.</p>
 							</div>
 						</div>
+
+						<div class="item">
+							<div class="col izq">
+								<h3>DSS04. Gestionar la continuidad</h3>
+								<p>Establecer y mantener un plan que permita responder a incidentes 
+									e interrupciones de servicio y mantener la disponibilidad de la 
+									información a un nivel aceptable para la empresa.</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="col izq">
+								<h3>DSS05. Gestionar servicios de seguridad</h3>
+								<p>Establecer y mantener los roles de seguridad y privilegios de 
+									acceso de la información y realizar la supervisión de la seguridad.</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="col izq">
+								<h3>DSS06. Gestionar controles de procesos de negocio</h3>
+								<p>Definir y mantener controles apropiados de proceso de negocio 
+									para asegurar que la información relacionada y procesada dentro 
+									de la organización satisface todos los requerimientos relevantes 
+									para el control de la información. Identificar los requisitos de 
+									control de la información y gestionar y operar los controles 
+									adecuados para asegurar que la información y su procesamiento 
+									satisfacen estos requerimientos.</p>
+							</div>
+						</div>
+
 					</div> 
 					<!-- FIN PROCESOS -->
-                    <!-- OTRA SECCION -->
-						<div id="controles">
+                    <!-- PRACTICAS-->
+						    <div id="controles">
+						    <div class="item">
+							    <div class="col izq">
+								    <h3>Prácticas de Gestión</h3>
+							    </div>
+						    </div>
+
+							<!-- PROCESO 1 -->
+							<div class="item">
+							    <div class="col izq">
+								    <h4>DSS01. Gestionar operaciones</h4>
+							    </div>
+						    </div>
 							<div class="item">
 								<div class="col izq">
-									<h3>Lorem ipsum dolor.</h3>
-									<p>Lorem ipsum dolor, sit amet consectetur, adipisicing elit.</p>
+									<h3>DSS01.01 Ejecutar procedimientos operativos</h3>
+									<p>Mantener y ejecutar tareas operativas de forma confiable y consistente.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS01.02 Gestionar servicios externalizados de TI</h3>
+									<p>Para mantener la protección de la información empresarial y la 
+										confiabilidad de la entrega del servicio.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS01.03 Supervisar la infraestructura de TI</h3>
+									<p>Almacenar información cronológica en los registros de operaciones para 
+										permitir la revisión de las operaciones y las actividades relacionadas 
+										con el soporte de las mismas.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS01.04 Gestionar el entorno</h3>
+									<p>Mantener las medidas para la protección contra factores ambientales.</p>
+								</div>
+							</div>							
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS01.05 Gestionar las instalaciones</h3>
+									<p>Incluyendo equipos de electricidad y comunicaciones, requerimientos 
+										técnicos y seguridad en el trabajo.</p>
 								</div>
 							</div>
 
+							<!-- PROCESO 2 -->
+							<div class="item">
+							    <div class="col izq">
+								    <h4>DSS02. Gestionar peticiones e incidentes de servicio</h4>
+							    </div>
+						    </div>
 							<div class="item">
 								<div class="col izq">
-									<h3>Donec placerat dignissin.</h3>
-									<p>Macenas iaculis aliquam lectus, coutywe.</p>
+									<h3>DSS02.01 Definir esquemas de clasificación de incidentes y peticiones de servicio</h3>
+									<p>Definir esquemas y modelos de clasificación de incidentes y peticiones de servicio.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS02.02 Registrar, clasificar y priorizar peticiones e incidentes</h3>
+									<p>Identificar y clasificar peticiones e incidentes, para luego asignar una prioridad 
+										según la criticidad del negocio y los acuerdos de servicio.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS02.03 Verificar, aprobar y resolver peticiones de servicio</h3>
+									<p>Seleccionar los procedimientos adecuados para peticiones y verificar que las 
+										peticiones de servicio cumplen los criterios de petición definidos.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS02.04 Investigar, diagnosticar y localizar incidentes</h3>
+									<p>Identificar y registrar síntomas de incidentes, determinar posibles causas 
+										y asignar recursos a su resolución.</p>
+								</div>
+							</div>							
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS02.05 Resolver y recuperarse ante incidentes</h3>
+									<p>Documentar, solicitar y probar las soluciones identificadas y ejecutar 
+										acciones de recuperación para restaurar el servicio TI relacionado.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS02.06 Cerrar peticiones de servicio e incidentes</h3>
+									<p>Verificar la satisfactoria resolución de incidentes y cierre.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS02.07 Seguir el estado y emitir informes</h3>
+									<p>Hacer seguimiento, analizar e informar de incidentes regularmente 
+										para proporcionar información para la mejora continua.</p>
 								</div>
 							</div>
 
+							<!-- PROCESO 3 -->
+							<div class="item">
+							    <div class="col izq">
+								    <h4>DSS03. Gestionar problemas</h4>
+							    </div>
+						    </div>
 							<div class="item">
 								<div class="col izq">
-									<h3>Nulla vestibulum.</h3>
-									<p>Lorem ipsum dolor, sit amet consectetur, adipisicing elit.</p>
+									<h3>DSS03.01 Identificar y clasificar problemas</h3>
+									<p>Definir e implementar criterios y procedimientos para informar de 
+										los problemas identificados.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS03.02 Investigar y diagnosticar problemas.</h3>
+									<p>Consultando a expertos en las materias relevantes para valorar y 
+										analizar las causas raíz.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS03.03 Levantar errores conocidos.</h3>
+									<p>Tan pronto como las causas raíz de los problemas se hayan identificado, 
+										crear registros de errores conocidos y una solución temporal apropiada.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS03.04 Resolver y cerrar problemas</h3>
+									<p>Identificar e iniciar soluciones sostenibles mediante el proceso de 
+										gestión de cambios. </p>
+								</div>
+							</div>							
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS03.05 Realizar una gestión de problemas proactiva.</h3>
+									<p>Recoger y analizar datos operacionales para identificar tendencias 
+										emergentes que puedan indicar problemas. </p>
 								</div>
 							</div>
 
+							<!-- PROCESO 4 -->
+							<div class="item">
+							    <div class="col izq">
+								    <h4>DSS04. Gestionar la continuidad</h4>
+							    </div>
+						    </div>
 							<div class="item">
 								<div class="col izq">
-									<h3>Sed semper.</h3>
-									<p> Voluptatibus magni amet laborum tenetur, eaque unde.</p>
+									<h3>DSS04.01 Definir la política de continuidad de negocio, objetivos y alcance.</h3>
+									<p>Definir la política y alcance de continuidad de negocio alineada con los objetivos 
+										de negocio y las partes interesadas.</p>
 								</div>
 							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS04.02 Mantener una estrategia de continuidad.</h3>
+									<p>Evaluar y escoger una estrategia de continuidad viable y efectiva en coste, que 
+										pueda asegurar la continuidad y recuperación de la empresa frente a un desastre.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS04.03 Desarrollar e implementar una respuesta a la continuidad del negocio</h3>
+									<p>Desarrollar un plan de continuidad de negocio (BCP) basado en la estrategia que 
+										documente los procedimientos y la información lista para el uso en un incidente.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS04.04 Ejercitar, probar y revisar el BCP</h3>
+									<p>Probar los acuerdos de continuidad regularmente para ejercitar los planes de 
+										recuperación y pueda permitir el desarrollo de soluciones innovadoras.</p>
+								</div>
+							</div>							
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS04.05 Revisar, mantener y mejorar el plan de continuidad</h3>
+									<p>Gestionar los cambios en el plan para asegurar que el plan de continuidad se 
+										mantiene actualizado y refleja los requerimientos actuales del negocio.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS04.06 Proporcionar formación en el plan de continuidad</h3>
+									<p>Proporcionar a todas las partes implicadas, sobre las sesiones formativas que 
+										contemplen los procedimientos, roles y responsabilidades en caso de disrupción.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS04.07 Gestionar acuerdos de respaldo</h3>
+									<p>Mantener la disponibilidad de la información crítica del negocio</p>
+								</div>
+							</div>
+
+							<!-- PROCESO 5 -->
+							<div class="item">
+							    <div class="col izq">
+								    <h4>DSS05. Gestionar servicios de seguridad</h4>
+							    </div>
+						    </div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS05.01 Proteger contra software malicioso (malware)</h3>
+									<p>Implementar y mantener medidas preventivas, de detección y correctivas en la empresa 
+										para proteger los sistemas de información y tecnología del software malicioso.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS05.02 Gestionar la seguridad de la red y las conexiones</h3>
+									<p>Utilizar medidas de seguridad y procedimientos de gestión para proteger la 
+										información en todos los modos de conexión.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS05.03 Gestionar la seguridad de los puestos de usuario final</h3>
+									<p>Asegurar que los puestos de usuario final están asegurados al nivel definido en los 
+										requerimientos de seguridad de la información procesada, almacenada o transmitida.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS05.04 Gestionar la identidad del usuario y el acceso lógico</h3>
+									<p>Asegurar que todos los usuarios tengan derechos de acceso a la información de 
+										acuerdo con los requerimientos de negocio.</p>
+								</div>
+							</div>							
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS05.05 Gestionar el acceso físico a los activos de TI</h3>
+									<p>Definir e implementar procedimientos para conceder, limitar y revocar acceso a 
+										locales, edificios y áreas de acuerdo con las necesidades del negocio.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS05.06 Gestionar documentos sensibles y dispositivos de salida</h3>
+									<p>Establecer salvaguardas físicas apropiadas y gestión del inventario para activos 
+										de TI sensibles.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS05.07 Supervisar la infraestructura para detectar eventos relacionados con la seguridad</h3>
+									<p>Usando herramientas de detección de intrusiones, supervisar la infraestructura para 
+										detectar accesos no autorizados y asegurar la supervisión general de eventos.</p>
+								</div>
+							</div>
+
+							<!-- PROCESO 6 -->
+							<div class="item">
+							    <div class="col izq">
+								    <h4>DSS06. Gestionar controles de procesos de negocio</h4>
+							    </div>
+						    </div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS06.01 Alinear actividades de control embebidas en los procesos de negocio con los objetivos corporativos</h3>
+									<p>Evaluar y supervisar continuamente la ejecución de actividades de los procesos de negocio y 
+										controles relacionados, basados en el riesgo corporativo.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS06.02 Controlar el procesamiento de la información</h3>
+									<p>Operar la ejecución de las actividades del proceso de negocio y controles para asegurar que el 
+										procesamiento de la información es válido, preciso y seguro.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS06.03 Gestionar roles, responsabilidades, privilegios de acceso y niveles de autorización</h3>
+									<p>Asegura que el negocio sepa donde están los datos y quién los está manejando en su nombre.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS06.04 Gestionar errores y excepciones</h3>
+									<p>Incluir el escalamiento de los errores y excepciones , y la ejecución de acciones correctivas. 
+										Esto proporciona garantía de precisión e integridad del proceso de información del negocio.</p>
+								</div>
+							</div>							
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS06.05 Asegurar la trazabilidad de los eventos, responsabilidades y de informaciónX</h3>
+									<p>Asegurar que la información de negocio pueda ser rastreada hasta los responsables y eventos 
+										de negocio que la originan.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col izq">
+									<h3>DSS06.06 Asegurar los activos de información</h3>
+									<p>Asegurar los activos de información accesibles por el negocio a través de los métodos aprobados, 
+										incluyendo la información en formato electrónico y físico.</p>
+								</div>
+							</div>
+							
 						</div>
 					<!-- FIN SECCION -->
 					</div>
